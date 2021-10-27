21bba49340c417b64a96de9921a9ca3ac59c1180
owenwong <owen.cq.cn@gmail.com>
2021-10-27 11:18:23 +0800

    chore: update case

--- scrape_url/src/main.rs
+++ scrape_url/src/main.rs
@@ -1,3 +1,18 @@
+/*
+ * @Descripttion:
+ * @Author: OwenWong
+ * @Email: owen.cq.cn@gmail.com
+ * @Date: 2021-10-25 20:17:21
+ */
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
+    print!("Fetching url: {}", url);
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    println!("Converting html to markdown...");
+    let md = html2md::parse_html(&body);
+    fs::write(output, md.as_bytes()).unwrap();
+    println!("Converted markdown has been saved in {}.", output);
 }
